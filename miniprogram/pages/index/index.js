5d9f3cadda46449032bfc790ea2aa52567826f66
zg-wujiahao <62708377+zg-wujiahao@users.noreply.github.com>
2020-04-22 19:51:58 +0800

    完成

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,100 +1,81 @@
 //index.js
 const app = getApp()
-
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    img1: '',
+    img2: '',
+    img3: '',
+    img4: '',
+    width:600,
+    height:600
   },
-
-  onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-  },
-
   // 上传图片
   doUpload: function() {
+    var that = this;
     // 选择图片
     wx.chooseImage({
       count: 1,
       sizeType: ['compressed'],
       sourceType: ['album', 'camera'],
       success: function(res) {
+        console.log(res)
         wx.showLoading({
           title: '上传中',
         })
-
         const filePath = res.tempFilePaths[0]
-
         // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        const cloudPath = 'Hi-image' + filePath.match(/\.[^.]+?$/)[0]
+        //console.log(cloudPath)
         wx.cloud.uploadFile({
           cloudPath,
           filePath,
-          success: res => {
+          success: (res) => {
+            let fileID = res.fileID
             console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
+            
+            //调用图片审查
+            wx.cloud.callFunction({
+              name: "reviewimage",
+              data: {
+                cloudPath: cloudPath
+              }
+            }).then((res) => {
+              console.log(res)
+              //console.log(res.result.AdsInfo.HitFlag)
+              if (res.result.AdsInfo.HitFlag === 0 && res.result.PoliticsInfo.HitFlag === 0 && res.result.PornInfo.HitFlag === 0 && res.result.TerroristInfo.HitFlag === 0) {
+                
+                //调用智能剪裁
+                wx.cloud.callFunction({
+                  name: "cutpicture",
+                  data: {
+                    fileID: fileID,
+                    pixel: [{
+                      width: 100,
+                      height: 100
+                    }, {
+                      width: 300,
+                      height: 202
+                    }, {
+                      width: 160,
+                      height: 88
+                    }]
+                  }
+                }).then(res => {
+                  console.log(res)
+                  that.setData({
+                    img1: fileID,
+                    img2: res.result[0],
+                    img3: res.result[1],
+                    img4: res.result[2],
+                    height:400
+                  })
+                })
+              } else {
+                wx.showToast({
+                  icon: 'none',
+                  title: '您上传的图片有敏感信息，请重新上传',
+                })
+              }
             })
           },
           fail: e => {
